90320c8d4924e09eaa99c3fc7cb8f5f74aad6593
RayceeM <mwatelaraycee33@gmail.com>
2018-12-14 11:05:25 +0300

    Change

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,16 +1,31 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [guestbook.views.layout :as layout]))
+            [guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
 
-(defn home []
-  (layout/common [:h1 "Guestbook"]
+(defn show-guests[])
+[:ul.guests
+(for [{:keys [message name timestamp]}
+[{:message "Hey work smart" :name "Racheal" :timestamp nil} 
+{:message "Awesome stuff" :name "Getty" :timestamp nil }]]
+[:li 
+[:blockquote message]
+[:p "-" ]])]
+(defn home [$ [name message error]]
+  (layout/common 
+  [:h1 "Guestbook"]
   [:p "Welcome visitor"]
+  [:p "error"]
+  ;generate list of comments
+  (show-guests)
   [:hr]
-  [:form
+  (form-to [post "/"]
   [:p "Name:"]
-  [:input]
+  (text-field "name" name)
   [:p "Message:"]
-  [:textarea {:rows 10 :cols 40}]]))
+  (:textarea {:rows 10 :cols 40} "message" message)
+  [:br]
+  (submit-button "comment"))))
 
 (defroutes home-routes
   (GET "/" [] (home)))
